da2882c5871e08852353b8742474a30f2c5f5965
Marcel <marcelb19@gmail.com>
2019-12-18 13:30:57 -0800

    WIP commit

--- day3/challenge1.js
+++ day3/challenge1.js
@@ -1,6 +1,29 @@
 import { promises as fs } from 'fs';
 import * as os from 'os';
 
+function getNextCoordinateStep(direction, movementMagnitude) {
+    switch(direction) {
+        case 'U':
+            return {
+                x: 0,
+                y: movementMagnitude
+            }
+        case 'R':
+            return {
+                x: 0,
+                y: movementMagnitude
+            }
+        case 'D':
+            return {
+                x: 0,
+                y: movementMagnitude
+            }
+        case 'L':
+            break;
+    }
+
+}
+
 const readWirePaths = async (inputFilename) => {
     let fileContent = await fs.readFile(inputFilename, 'utf8');
     return fileContent
@@ -13,6 +36,12 @@ readWirePaths('input.txt')
         let result = inputWires.map((inputWire) => {
             return inputWire.split(',');
         })
-
-        console.log(result);
+        .map((wireSteps) => {
+            let currentX = 0;
+            let currentY = 0;
+            return wireSteps.map((wireStep) => {
+                let direction = wireStep[0];
+                let movementMagnitude = parseInt(wireStep.slice(1));
+            })
+        })
     }). catch(err => console.log(err));
